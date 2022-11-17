064583f9a26ce2e582c101c7c8aa69ab63eee16d
alanrceratti <99280773+alanrceratti@users.noreply.github.com>
2022-11-17 13:41:38 +0000

    login and forms

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,42 +1,51 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import useForm from "../../Hooks/useForm";
+import Button from "../Forms/Button";
+import Input from "../Forms/Input";
 
 const LoginForm = () => {
-	const [username, setUsername] = React.useState("");
-	const [password, setPassword] = React.useState("");
+	const username = useForm("email");
+	const password = useForm();
 
 	function handleSubmit(event) {
 		event.preventDefault();
-		fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({ username, password }),
-		})
-			.then((response) => {
-				return response.json();
+
+		if (username.validate() && password.validate()) {
+			fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(),
 			})
-			.then((json) => {
-				console.log(json);
-			});
+				.then((response) => {
+					console.log(response);
+					return response.json();
+				})
+				.then((json) => {
+					console.log(json);
+				});
+		}
 	}
 
 	return (
 		<section>
 			<h1>Login</h1>
 			<form action="" onSubmit={handleSubmit}>
-				<input
+				<Input
+					label="Usuário"
 					type="text"
-					valeu={username}
-					onChange={({ target }) => setUsername(target.value)}
+					name="username"
+					{...username}
 				/>
-				<input
-					type="text"
-					valeu={password}
-					onChange={({ target }) => setPassword(target.value)}
+				<Input
+					label="Senha"
+					type="password"
+					name="password"
+					{...password}
 				/>
-				<button>Entrar</button>
+				<Button>Entrar</Button>
 			</form>
 			<Link to="/login/criar">Cadastro</Link>
 		</section>
