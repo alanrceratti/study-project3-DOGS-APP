29278280f4242949ddf2065e7abde67049932ca2
alanrceratti <99280773+alanrceratti@users.noreply.github.com>
2022-11-19 19:36:16 +0000

    mobile css + menu opening

--- src/Components/User/UserPhotoPost.js
+++ src/Components/User/UserPhotoPost.js
@@ -1,7 +1,31 @@
 import React from "react";
+import Button from "../Forms/Button";
+import Input from "../Forms/Input";
+import styles from "./UserPhotoPost.module.css";
 
 const UserPhotoPost = () => {
-	return <div>Postar Foto</div>;
+	function handleSubmit(event) {
+		event.preventDefault();
+	}
+
+	function handleImgChange() {}
+
+	return (
+		<section className={`${styles.photoPost} animeLeft`}>
+			<Input label="Nome" type="text" name="nome"></Input>
+			<Input label="Peso" type="text" name="peso"></Input>
+			<Input label="Idade" type="text" name="idade"></Input>
+			<Input
+				type="file"
+				name="img"
+				id="img"
+				onChange={handleImgChange}
+			></Input>
+
+			<Button />
+			<form onSubmit={handleSubmit}></form>
+		</section>
+	);
 };
 
 export default UserPhotoPost;
