701e6fded5617deffd83ddb48f1fe3c91f5aeda9
alanrceratti <99280773+alanrceratti@users.noreply.github.com>
2022-11-19 11:04:27 +0000

    login from api

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -4,28 +4,42 @@ import useForm from "../../Hooks/useForm";
 import Button from "../Forms/Button";
 import Input from "../Forms/Input";
 import { UserContext } from "../../UserContext";
-import { TOKEN_POST } from "../../api";
+// import { TOKEN_POST, USER_GET } from "../../api";
 
 const LoginForm = () => {
 	const username = useForm();
 	const password = useForm();
 	const { userLogin } = React.useContext(UserContext);
 
-	
+	// React.useEffect(() => {
+	// 	const token = window.localStorage.getItem("token");
+	// 	if (token) {
+	// 		getUser(token);
+	// 	}
+	// }, []);
+
+	// async function getUser(token) {
+	// 	const { url, options } = USER_GET(token);
+	// 	const response = await fetch(url, options);
+	// 	const json = await response.json();
+	// 	console.log(json);
+	// }
 
 	async function handleSubmit(event) {
 		event.preventDefault();
 
 		if (username.validate() && password.validate()) {
-			const { url, options } = TOKEN_POST({
-				username: username.value,
-				password: password.value,
-			});
+			userLogin(username.value, password.value);
 
-			const response = await fetch(url, options);
-			const json = await response.json();
-			window.localStorage.setItem('token', json.token);
-			console.log(json);
+			// const { url, options } = TOKEN_POST({
+			// 	username: username.value,
+			// 	password: password.value,
+			// });
+
+			// const response = await fetch(url, options);
+			// const json = await response.json();
+			// window.localStorage.setItem("token", json.token);
+			// getUser(json.token);
 		}
 	}
 
