e394037c856793cef2f3ea6cd3e2e3ef73a4367a
alanrceratti <99280773+alanrceratti@users.noreply.github.com>
2022-11-19 17:33:47 +0000

    user page styling + routes

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
+import ProtectedRoute from "./Components/Helper/ProtectedRoute";
 import Home from "./Components/Home";
 import Login from "./Components/Login/Login";
-import { UserStorage } from './UserContext';
+import User from "./Components/User/User";
+import { UserStorage } from "./UserContext";
 
 function App() {
 	return (
@@ -15,7 +17,16 @@ function App() {
 					<Header />
 					<Routes>
 						<Route path="/" element={<Home />} />
-						<Route path="/login/*" element={<Login />} />
+						<Route path="login/*" element={<Login />} />
+						<Route
+							path="conta/*"
+							element={
+								// protectedroute para usuario nao logado nao ter acesso aos dados
+								<ProtectedRoute>
+									<User />
+								</ProtectedRoute>
+							}
+						/>
 					</Routes>
 					<Footer />
 				</UserStorage>
